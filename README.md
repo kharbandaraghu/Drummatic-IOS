fc53c930baa6753e7f98809b7a919114b151bb2a
Raghav Kharbanda <kharbandaraghu@gmail.com>
2021-02-20 13:25:46 -0500

    update

---
+++ README.md
@@ -0,0 +1,15 @@
+# Drummatic - IOS
+
+A simple IOS application that lets you play drums on your phone like an EDM Drum Pad
+
+<br/>
+
+## Screenshots
+
+<br/>
+
+![Screenshot](ss1.png)
+
+<br/>
+
+![Screenshot](ss2.png)
--- drummatic/ViewController.swift
+++ drummatic/ViewController.swift
@@ -6,12 +6,57 @@
 //
 
 import UIKit
+import AVFoundation
+var player: AVAudioPlayer?
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var beatButton: [UIButton]!
+    @IBAction func bIn(_ sender: UIButton) {
+        (sender as UIButton).backgroundColor = UIColor.darkGray
+        playSound(nam: (sender.titleLabel?.text)!)
+    }
+    
+    @IBAction func bpress(_ sender: UIButton) {
+        (sender as UIButton).backgroundColor = UIColor.clear
+    }
+    
+
+    func playSound(nam: String) {
+        guard let url = Bundle.main.url(forResource: "sounds/"+nam, withExtension: "aif") else {
+            print("url not found")
+            return
+        }
+
+        do {
+            /// this codes for making this app ready to takeover the device audio
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /// change fileTypeHint according to the type of your audio file (you can omit this)
+
+            /// for iOS 11 onward, use :
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /// else :
+            /// player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3)
+
+            // no need for prepareToPlay because prepareToPlay is happen automatically when calling play()
+            player!.play()
+        } catch let error as NSError {
+            print("error: \(error.localizedDescription)")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // set border arounf
+        for i in beatButton{
+            i.layer.borderColor = UIColor.darkGray.cgColor
+            i.layer.borderWidth = 3.0
+            i.layer.cornerRadius = 20.0
+        }
+        
     }
 
 
